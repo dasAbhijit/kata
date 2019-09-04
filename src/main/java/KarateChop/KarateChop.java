ca830c5a8e016874ff39d56fdbeca7b4dfcded87
dasAbhijit <abhijit18aug@gmail.com>
2019-09-04 18:57:10 +0530

    initial commit

---
+++ src/main/java/KarateChop/KarateChop.java
@@ -0,0 +1,129 @@
+package KarateChop;
+
+import java.util.Arrays;
+
+public class KarateChop {
+
+    public int chop1(int value, int[] sortedArray) {
+        return linerSearch(value, sortedArray);
+    }
+
+    private int linerSearch(int value, int[] sortedArray) {
+        for (int index = 0; index < sortedArray.length; index++) {
+            if (value == sortedArray[index]) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public int chop2(int value, int[] sortedArray) {
+        return recSearch(sortedArray, 0, sortedArray.length - 1, value);
+    }
+
+    private int recSearch(int[] sortedArray, int left, int right, int value) {
+        if (right < left)
+            return -1;
+        if (sortedArray[left] == value)
+            return left;
+        if (sortedArray[right] == value)
+            return right;
+        return recSearch(sortedArray, left + 1, right - 1, value);
+    }
+
+    public int chop3(int value, int[] sortedArray) {
+        return binarySearch(sortedArray, 0, sortedArray.length - 1, value);
+    }
+
+    private int binarySearch(int arr[], int left, int right, int value) {
+        if (right >= left) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == value)
+                return mid;
+            if (arr[mid] > value)
+                return binarySearch(arr, left, mid - 1, value);
+            return binarySearch(arr, mid + 1, right, value);
+        }
+        return -1;
+    }
+
+    public int chop4(int value, int[] sortedArray) {
+        return jumpSearch(sortedArray, 2, value);
+    }
+
+    private int jumpSearch(int arr[], int jumpBy, int value) {
+        int jumpIndex;
+        for (jumpIndex = 0; jumpIndex <= arr.length - 1; jumpIndex = jumpIndex + jumpBy) {
+            if (arr[jumpIndex] == value) {
+                return jumpIndex;
+            }
+            if (arr[jumpIndex] > value) {
+                int startIndex = jumpIndex - jumpBy;
+                if (startIndex < 0) startIndex = 0;
+                return iterativeSearchFromIndex(startIndex, jumpIndex, arr, value);
+            }
+        }
+        if (jumpIndex >= arr.length && arr.length != 0) {
+            return iterativeSearchFromIndex(jumpIndex - jumpBy, arr.length - 1, arr, value);
+        }
+
+        return -1;
+    }
+
+    private int iterativeSearchFromIndex(int fromIndex, int toIndex, int[] sortedArray, int value) {
+        for (int index = fromIndex; index <= toIndex; index++) {
+            if (value == sortedArray[index]) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public int chop5(int value, int[] sortedArray) {
+        return binarySearchLibrary(value, sortedArray);
+    }
+
+    private int binarySearchLibrary(int value, int[] sortedArray) {
+        if (Arrays.binarySearch(sortedArray, value) < 0) {
+            return -1;
+        }
+        return Arrays.binarySearch(sortedArray, value);
+    }
+
+    public int chop6(int value, int[] sortedArray) {
+        Search iterativeSearch = (int v, int[] arr) -> {
+            for (int index = 0; index < arr.length; index++) {
+                if (v == arr[index]) {
+                    return index;
+                }
+            }
+            return -1;
+        };
+        return iterativeSearch.search(value, sortedArray);
+    }
+
+    public int chop7(int value, int[] sortedArray) {
+        return binarySearchWithArraySlicingRecursion(value, sortedArray);
+    }
+
+    private int binarySearchWithArraySlicingRecursion(int value, int[] sortedArray) {
+        if (sortedArray.length <= 0) {
+            return -1;
+        }
+        int mid = sortedArray.length / 2;
+        if (sortedArray[mid] == value) {
+            return mid;
+        } else if (sortedArray[mid] < value && mid + 1 <= sortedArray.length - 1) {
+            binarySearchWithArraySlicingRecursion(value, Arrays.copyOfRange(sortedArray, mid + 1, sortedArray.length - 1));
+        } else if (sortedArray[mid] > value && mid - 1 >= 0) {
+            binarySearchWithArraySlicingRecursion(value, Arrays.copyOfRange(sortedArray, 0, mid - 1));
+        }
+        return -1;
+    }
+
+    @FunctionalInterface
+    interface Search {
+        int search(int value, int[] sortedArray);
+    }
+
+}
---
+++ src/test/java/KarateChop/testKarateChop.java
@@ -0,0 +1,194 @@
+package KarateChop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testKarateChop {
+    private KarateChop karateChop = new KarateChop();
+    private int[] arrayLengthZero = {};
+    private int[] arrayLengthOne = {1};
+    private int[] arrayLengthThree = {1, 3, 5};
+    private int[] arrayLengthFour = {1, 3, 5, 7};
+
+    @Test
+    public void testChop1() {
+        Assert.assertEquals(-1, karateChop.chop1(3, arrayLengthZero));
+
+        Assert.assertEquals(-1, karateChop.chop1(3, arrayLengthOne));
+        Assert.assertEquals(0, karateChop.chop1(1, arrayLengthOne));
+
+        Assert.assertEquals(0, karateChop.chop1(1, arrayLengthThree));
+        Assert.assertEquals(1, karateChop.chop1(3, arrayLengthThree));
+        Assert.assertEquals(2, karateChop.chop1(5, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop1(0, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop1(2, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop1(4, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop1(6, arrayLengthThree));
+
+        Assert.assertEquals(0, karateChop.chop1(1, arrayLengthFour));
+        Assert.assertEquals(1, karateChop.chop1(3, arrayLengthFour));
+        Assert.assertEquals(2, karateChop.chop1(5, arrayLengthFour));
+        Assert.assertEquals(3, karateChop.chop1(7, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop1(0, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop1(2, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop1(4, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop1(6, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop1(8, arrayLengthFour));
+    }
+
+    @Test
+    public void testChop2() {
+        Assert.assertEquals(-1, karateChop.chop2(3, arrayLengthZero));
+
+        Assert.assertEquals(-1, karateChop.chop2(3, arrayLengthOne));
+        Assert.assertEquals(0, karateChop.chop2(1, arrayLengthOne));
+
+        Assert.assertEquals(0, karateChop.chop2(1, arrayLengthThree));
+        Assert.assertEquals(1, karateChop.chop2(3, arrayLengthThree));
+        Assert.assertEquals(2, karateChop.chop2(5, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop2(0, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop2(2, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop2(4, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop2(6, arrayLengthThree));
+
+        Assert.assertEquals(0, karateChop.chop2(1, arrayLengthFour));
+        Assert.assertEquals(1, karateChop.chop2(3, arrayLengthFour));
+        Assert.assertEquals(2, karateChop.chop2(5, arrayLengthFour));
+        Assert.assertEquals(3, karateChop.chop2(7, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop2(0, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop2(2, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop2(4, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop2(6, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop2(8, arrayLengthFour));
+    }
+
+    @Test
+    public void testChop3() {
+        Assert.assertEquals(-1, karateChop.chop3(3, arrayLengthZero));
+
+        Assert.assertEquals(-1, karateChop.chop3(3, arrayLengthOne));
+        Assert.assertEquals(0, karateChop.chop3(1, arrayLengthOne));
+
+        Assert.assertEquals(0, karateChop.chop3(1, arrayLengthThree));
+        Assert.assertEquals(1, karateChop.chop3(3, arrayLengthThree));
+        Assert.assertEquals(2, karateChop.chop3(5, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop3(0, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop3(2, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop3(4, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop3(6, arrayLengthThree));
+
+        Assert.assertEquals(0, karateChop.chop3(1, arrayLengthFour));
+        Assert.assertEquals(1, karateChop.chop3(3, arrayLengthFour));
+        Assert.assertEquals(2, karateChop.chop3(5, arrayLengthFour));
+        Assert.assertEquals(3, karateChop.chop3(7, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop3(0, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop3(2, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop3(4, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop3(6, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop3(8, arrayLengthFour));
+    }
+
+    @Test
+    public void testChop4() {
+        Assert.assertEquals(-1, karateChop.chop4(3, arrayLengthZero));
+
+        Assert.assertEquals(-1, karateChop.chop4(3, arrayLengthOne));
+        Assert.assertEquals(0, karateChop.chop4(1, arrayLengthOne));
+
+        Assert.assertEquals(0, karateChop.chop4(1, arrayLengthThree));
+        Assert.assertEquals(1, karateChop.chop4(3, arrayLengthThree));
+        Assert.assertEquals(2, karateChop.chop4(5, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop4(0, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop4(2, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop4(4, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop4(6, arrayLengthThree));
+
+        Assert.assertEquals(0, karateChop.chop4(1, arrayLengthFour));
+        Assert.assertEquals(1, karateChop.chop4(3, arrayLengthFour));
+        Assert.assertEquals(2, karateChop.chop4(5, arrayLengthFour));
+        Assert.assertEquals(3, karateChop.chop4(7, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop4(0, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop4(2, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop4(4, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop4(6, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop4(8, arrayLengthFour));
+    }
+
+    @Test
+    public void testChop5() {
+        Assert.assertEquals(-1, karateChop.chop5(3, arrayLengthZero));
+
+        Assert.assertEquals(-1, karateChop.chop5(3, arrayLengthOne));
+        Assert.assertEquals(0, karateChop.chop5(1, arrayLengthOne));
+
+        Assert.assertEquals(0, karateChop.chop5(1, arrayLengthThree));
+        Assert.assertEquals(1, karateChop.chop5(3, arrayLengthThree));
+        Assert.assertEquals(2, karateChop.chop5(5, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop5(0, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop5(2, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop5(4, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop5(6, arrayLengthThree));
+
+        Assert.assertEquals(0, karateChop.chop5(1, arrayLengthFour));
+        Assert.assertEquals(1, karateChop.chop5(3, arrayLengthFour));
+        Assert.assertEquals(2, karateChop.chop5(5, arrayLengthFour));
+        Assert.assertEquals(3, karateChop.chop5(7, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop5(0, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop5(2, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop5(4, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop5(6, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop5(8, arrayLengthFour));
+    }
+
+    @Test
+    public void testChop6() {
+        Assert.assertEquals(-1, karateChop.chop6(3, arrayLengthZero));
+
+        Assert.assertEquals(-1, karateChop.chop6(3, arrayLengthOne));
+        Assert.assertEquals(0, karateChop.chop6(1, arrayLengthOne));
+
+        Assert.assertEquals(0, karateChop.chop6(1, arrayLengthThree));
+        Assert.assertEquals(1, karateChop.chop6(3, arrayLengthThree));
+        Assert.assertEquals(2, karateChop.chop6(5, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop6(0, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop6(2, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop6(4, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop6(6, arrayLengthThree));
+
+        Assert.assertEquals(0, karateChop.chop6(1, arrayLengthFour));
+        Assert.assertEquals(1, karateChop.chop6(3, arrayLengthFour));
+        Assert.assertEquals(2, karateChop.chop6(5, arrayLengthFour));
+        Assert.assertEquals(3, karateChop.chop6(7, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop6(0, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop6(2, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop6(4, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop6(6, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop6(8, arrayLengthFour));
+    }
+
+    @Test
+    public void testChop7() {
+        Assert.assertEquals(-1, karateChop.chop7(3, arrayLengthZero));
+
+        Assert.assertEquals(-1, karateChop.chop7(3, arrayLengthOne));
+        Assert.assertEquals(0, karateChop.chop7(1, arrayLengthOne));
+
+        Assert.assertEquals(0, karateChop.chop7(1, arrayLengthThree));
+        Assert.assertEquals(1, karateChop.chop7(3, arrayLengthThree));
+        Assert.assertEquals(2, karateChop.chop7(5, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop7(0, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop7(2, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop7(4, arrayLengthThree));
+        Assert.assertEquals(-1, karateChop.chop7(6, arrayLengthThree));
+
+        Assert.assertEquals(0, karateChop.chop7(1, arrayLengthFour));
+        Assert.assertEquals(1, karateChop.chop7(3, arrayLengthFour));
+        Assert.assertEquals(2, karateChop.chop7(5, arrayLengthFour));
+        Assert.assertEquals(3, karateChop.chop7(7, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop7(0, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop7(2, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop7(4, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop7(6, arrayLengthFour));
+        Assert.assertEquals(-1, karateChop.chop7(8, arrayLengthFour));
+    }
+}
